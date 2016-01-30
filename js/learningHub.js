6dc78fe313a67e2be975f87b2fd286f2bcf15e12
Niyas C <niyasc@localhost.localdomain>
2016-01-30 17:40:08 +0530

    Done for this challenge. Pagination is not implemented

--- js/learningHub.js
+++ js/learningHub.js
@@ -6,6 +6,26 @@ learningHub.controller('CourseController',['$scope', '$http', function($scope, $
   }, function(data) {
     alert("Failed to load json data");
   });
+
+  $scope.filteredTodos = $scope.results
+  ,$scope.currentPage = 1
+  ,$scope.numPerPage = 10
+  ,$scope.maxSize = 5;
+
+  $scope.makeTodos = function() {
+    $scope.todos = [];
+    for (i=1;i<=1000;i++) {
+      $scope.todos.push({ text:"todo "+i, done:false});
+    }
+  };
+  $scope.makeTodos();
+
+  $scope.$watch("currentPage + numPerPage", function() {
+    var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+    , end = begin + $scope.numPerPage;
+
+    $scope.filteredTodos = $scope.todos.slice(begin, end);
+  });
 }]);
 
 learningHub.directive('starRating', function () {
